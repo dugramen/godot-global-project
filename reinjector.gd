c031b9dcb471cb62954cb9e40e9421cbb316e08e
krahman <koliur.rahman@my.liu.edu>
2024-08-02 16:54:37 -0400

    initial commit & script injection

---
+++ reinjector.gd
@@ -0,0 +1,13 @@
+@tool
+
+static var scr: GDScript = preload("res://script.gd")
+
+static func _static_init() -> void:
+	var settings := EditorInterface.get_editor_settings()
+	var new_scr := GDScript.new()
+	new_scr.source_code = scr.source_code\
+		.trim_prefix("#")\
+		.replace("_PATH_TO_REPLACE_", ProjectSettings.globalize_path("res://"))
+	print(new_scr.source_code)
+	settings.set_setting("portable_plugins/injected_script", new_scr)
+	new_scr.reload()
---
+++ script.gd
@@ -0,0 +1,4 @@
+#@tool
+static var path := "_PATH_TO_REPLACE_"
+static func _static_init() -> void:
+	print("Injected static initiation @path ", path)
